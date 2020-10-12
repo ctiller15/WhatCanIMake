381e6440597a93eda73c7973a4e6adf541131c8c
Christopher <christiller2015@gmail.com>
2020-10-12 00:40:14 -0400

    Remove extra from app.js, move app.test.js to tests folder. Create test w/ expected failure for post-click results

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '../app/store';
+import App from '../App';
+
+test('renders search button', () => {
+	const { getByText } = render(
+		<Provider store={store}>
+			<App />
+		</Provider>
+	);
+
+	expect(getByText('Search')).toBeInTheDocument();
+});
+
+test('Upon search button being clicked, component renders new data if successful query', async () => {
+	
+	fireEvent.click(getByText('Search'))
+
+	const results = await screen.findAllByRole('div', {name: /list-item/i})
+
+	expect(results).toHaveLength(10)
+
+	throw new Error('Finish the test!');
+})
